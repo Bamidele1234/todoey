13af86300eb244e15f7df7ed2e85c83878386169
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-07-19 15:23:06 +0100

    Reached a checkpoint

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:todoey/widgets/task_tile.dart';
+import 'package:todoey/models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(
+      name: 'Wash Bathroom',
+    ),
+    Task(
+      name: 'Make School List',
+    ),
+    Task(
+      name: 'Clean car boot',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView(
--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,23 +1,55 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = true;
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: const Text(
+      leading: Text(
         'Buy Milk',
         style: TextStyle(
           fontSize: 18,
+          decoration: isChecked ? TextDecoration.lineThrough : null,
         ),
       ),
-      trailing: Checkbox(
-        value: true,
-        onChanged: (bool? checked) {},
+      trailing: TaskCheckBox(
+        checkedBoxState: isChecked,
+        callBackParam: (bool? newValue) {
+          setState(() {
+            isChecked = newValue!;
+          });
+        },
       ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  const TaskCheckBox({
+    Key? key,
+    required this.checkedBoxState,
+    required this.callBackParam,
+  }) : super(key: key);
+
+  final bool checkedBoxState;
+  final Function(bool?)? callBackParam;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkedBoxState,
+      onChanged: callBackParam,
     );
   }
 }
